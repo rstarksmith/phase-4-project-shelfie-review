a0673bedeee802af1c434631b14fe848048ac66c
Rachel <rstark.smith@gmail.com>
2023-02-13 14:46:52 -0600

    Add form to SignUp

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 const SignUp = () => {
   return (
-    <div>SignUpForm</div>
-  )
+    <div>
+      <div>
+        <h3> Sign in to My Shelfie</h3>
+        <form>
+          <input
+            type="text"
+            name="username"
+            // value={username}
+            // onChange={handleChange}
+            placeholder="Create Username..."
+          />
+          <br />
+          <input
+            type="text"
+            name="password"
+            // value={password}
+            // onChange={handleChange}
+            placeholder="Create Password..."
+          />
+          <br />
+          <input
+            type="text"
+            name="password"
+            // value={password}
+            // onChange={handleChange}
+            placeholder="Confirm Password..."
+          />
+          <br />
+          <input
+            type="text"
+            name="password"
+            // value={password}
+            // onChange={handleChange}
+            placeholder="Photo URL of your current TBR shelf..."
+          />
+        </form>
+
+        <p>Already have an account?</p>
+        <Link to="/signin">
+          <p>Sign in</p>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default SignUp
