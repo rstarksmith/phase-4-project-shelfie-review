43c3102a66e9d5932c2f38911c33367a38af3fdf
Rachel <rstark.smith@gmail.com>
2023-02-14 18:40:14 -0600

    Add create User and Error exception handle

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ApplicationRecord
     validates :title, :author, :genre, :image_url,  presence: true
+    validates :title, uniqueness: true
+    # custom validation -maybe have a special function saying that if the author and title match an existing instance, then not valid
     validates :title, :author, length: { maximum: 50 }
     # would like to add a default image for any book without?
     
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-    validates :username, presence: true, 
+    validates :username, 
+        presence: true, 
+        uniqueness: true,
         length: { in: 3..15 }, 
         format: { without: /\s/, message: "cannot contain whitespace" }
 
