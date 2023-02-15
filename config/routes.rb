bb8fbd4ea05632641913e1d044929a2ca131866a
Rachel <rstark.smith@gmail.com>
2023-02-14 18:57:25 -0600

    add authorization

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   resources :books, only: [:index, :show, :create] do
     resources :reviews
   end
-  resources :users, only: [:show, :create]
+  
+  get "/myshelfie", to: "users#show"
+  post "/signup", to: "users#create"
+  post "/signin", to: "sessions#create"
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
