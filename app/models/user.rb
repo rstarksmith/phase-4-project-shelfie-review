b7ae2aa7476942f6cc243a6c4a66e99233b629ab
Rachel <rstark.smith@gmail.com>
2023-03-13 14:05:31 -0500

    password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
-    validates :username, 
-        presence: true, 
-        uniqueness: true,
-        length: { in: 3..15 }, 
-        format: { without: /\s/, message: "cannot contain space" }
-
+    validates :username, presence: true, uniqueness: true,
+        length: { in: 3..15 }, format: { without: /\s/, message: "cannot contain space" }
+    validates :password, confirmation: true, length: {in: 3..20}
+    validates :password_confirmation, presence: true
     validates :photo_url, presence: true    
+
     has_many :reviews
     has_many :books, through: :reviews
 
