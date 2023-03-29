e19f633771b17469fd259702ebb6bc0094096b7e
Rachel <rstark.smith@gmail.com>
2023-03-29 12:56:51 -0500

    scope review order

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,7 @@ class Book < ApplicationRecord
     validates :title, uniqueness: true, length: { maximum: 50 }
     validates :author, length: { maximum: 50 }
 
-    has_many :reviews, dependent: :destroy
+    has_many :reviews, -> { order(created_at: :desc)}, dependent: :destroy
     has_many :users, through: :reviews
 
     # scope :by_title, -> { order(title: :asc) }
