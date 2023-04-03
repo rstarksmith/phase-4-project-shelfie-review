4157a561779a1029ba8f76ee7af54c33755dda5c
Rachel <rstark.smith@gmail.com>
2023-04-03 08:35:42 -0500

    refactor review Serializer

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -2,7 +2,7 @@ class ReviewSerializer < ActiveModel::Serializer
   attributes :id, :header, :comment, :book_id, :user_id, :owner
 
   def owner
-    "@#{self.object.user.username}"
+    "@#{object.user.username}"
   end
 
   belongs_to :user
